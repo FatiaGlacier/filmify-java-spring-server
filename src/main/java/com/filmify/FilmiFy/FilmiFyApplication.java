d326febf6bf0e1e6bfd992d8613638de3dea4024
FatiaGlacier <dmfatianov@gmail.com>
2023-11-02 17:14:25 +0200

    First commit Java Spring project added

---
+++ src/main/java/com/filmify/FilmiFy/FilmiFyApplication.java
@@ -0,0 +1,13 @@
+package com.filmify.FilmiFy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmiFyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmiFyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filmify/FilmiFy/FilmiFyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmify.FilmiFy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmiFyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
